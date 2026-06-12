cb4bdd44e90579610fb784fc344179489de72b20
dang <dangtrandang@gmail.com>
2026-06-12 15:00:48 +0700

    Convert line endings to LF and finalize scanned overlay UI with password layer

--- post-fs-data.sh
+++
@@ -1,8 +0,0 @@
-#!/system/bin/sh
-# Magisk post-fs-data Script
-# Family Link Lock Cleanup Module
-
-# This script runs in post-fs-data mode (before system services start).
-# No initialization tasks are needed for this module, but we log the boot hook.
-
-log -t FamilyLockModule "post-fs-data hook executed"
